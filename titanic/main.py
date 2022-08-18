a8de6f39e1842e6a3add64f8f6b5d757ceac71e8
Oscar <oscarhernandez@geovoy.com>
2022-08-18 09:22:23 -0500

    using fastapi its my hello world in ML

---
+++ titanic/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+import joblib
+import uvicorn
+
+
+app = FastAPI()
+
+@app.post('/api/predictions')
+def get_prediction():
+    return {'Hello motherfucker'}
+
+
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host = 'localhost', port=5000, reload = True)
